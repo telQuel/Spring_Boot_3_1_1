f7bc1d7f3269941cb374ed1c52ea4d1db851cdea
Ilya <efgvj45@mail.ru>
2022-06-03 12:49:00 +0300

    Add UserServiceImpl

--- src/main/java/com/example/sb_crud_3_1_1/service/UserService.java
+++ src/main/java/com/example/sb_crud_3_1_1/service/UserService.java
@@ -7,29 +7,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
 
-    private final UserRepository userRepository;
+public interface UserService {
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public List<User> findAll();
 
-    public List<User> findAll() {
-        return (List<User>) userRepository.findAll();
-    }
+    public User findById(int id);
 
-    public User findById(int id) {
-        return userRepository.findById(id).orElse(null);
-    }
+    public User saveUser(User user);
 
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteById(Integer id) {
-        userRepository.deleteById(id);
-    }
+    public void deleteById(Integer id);
 }
---
+++ src/main/java/com/example/sb_crud_3_1_1/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.sb_crud_3_1_1.service;
+
+import com.example.sb_crud_3_1_1.model.User;
+import com.example.sb_crud_3_1_1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public User findById(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
