1745283e08f75f86e395e4cabfff8ecd63f56a1d
Ilya <efgvj45@mail.ru>
2022-06-03 14:48:02 +0300

    Refactor UserServiceImpl

--- src/main/java/com/example/sb_crud_3_1_1/service/UserServiceImpl.java
+++ src/main/java/com/example/sb_crud_3_1_1/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.sb_crud_3_1_1.model.User;
 import com.example.sb_crud_3_1_1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,21 +18,25 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> findAll() {
         return (List<User>) userRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findById(int id) {
         return userRepository.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void deleteById(Integer id) {
         userRepository.deleteById(id);
     }
